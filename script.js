7a715b2fb72362ed57270ffeed04353645aee859
AnaLauraCavalheiroMiranda <ana.cavalheiro.miranda@escola.pr.gov.br>
2025-07-31 11:44:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function calcular(){
+    
+    let capital_inicial = parseInt(document.getElementById('capital_inicial').value);
+    let taxa = parseInt(document.getElementById('taxa').value);
+    let tempo = parseInt(document.getElementById('tempo').value);
+
+    let porcentagem_de_juro = taxa/100;
+    let juros = capital_inicial * porcentagem_de_juro * tempo;
+    let montante = juros + capital_inicial;
+
+    document.getElementById('Resultado_juros').textContent = "Juros: " + juros;
+    document.getElementById('Resultado_montante').textContent = "Montante: " + montante;
+
+}
