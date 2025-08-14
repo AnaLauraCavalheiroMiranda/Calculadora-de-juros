6b573fe3fd60c31f44a7a934fed68999b65ed6c9
AnaLauraCavalheiroMiranda <ana.cavalheiro.miranda@escola.pr.gov.br>
2025-08-14 08:48:29 -0300

    Add files via upload

---
+++ Cafe-main/cafe-main/script.js
@@ -0,0 +1,30 @@
+Terminar_pedido()
+{
+  //vetores de objeto
+
+const cafes = [
+
+  { id:"cafe_comum", nome: "cafe comum", preco: 2 }
+
+];
+
+const chocolates = [
+  { id:"chocolate", nome: "chocolate ao leite", preco: 3 }
+  { id:"chocolate_"}
+
+];
+}
+  {
+   document.getElementById('chocolate').textContent = "chocolate: " + chocolate;
+}
+
+//Parte para não usar a mesma opção "selecione" deixando em branco.
+  function removerPlaceholder(selectElement) {
+    const primeiraOpcao = selectElement.options[0];
+    }
+
+//tentando mostrar aprecer
+    {
+    alert (chocolate.preco)
+
+  }
